4a22b6a66c5141400e3aab56edc739bccda02ab0
Luis Felipe Carrasquero <41020323+FelipeCVSS@users.noreply.github.com>
2021-03-26 21:23:26 -0500

    Los que faltaron

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import App from './App'
+import App from './Containers/App'
+
 
 
 ReactDOM.render(
